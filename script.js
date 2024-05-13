1a871867c379eda77d86d9f46617874b5aa90bad
CorySands <145405020+CorySands@users.noreply.github.com>
2024-05-13 18:10:14 -0500

    Add files via upload
    Commit initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var fName = prompt("What is your First Name?"); // Asks user for their first name and saves the result to the fName variable.
+alert("Welcome, " + fName + "."); // Displays a message welcoming the user with the inputted first name.
+
+const piValue = 3.1415926; // Saves the value of pi to the constant piValue.
+
+var myFavNum = Number(prompt("What is your Favorite Number?")); // Asks user for their favorite number and converts the value to a number. Then, stores the result in the myFavNum variable.
+var myArea = piValue * myFavNum**2; // Calculates the area of a circle using myFavNum as the radius and piValue as pi. Then, stores the result in the myArea variable.
+
+alert("Hello " + fName + ", you entered " + myFavNum + " as your favorite number. If that was the radius of a circle, the area would be " + myArea + "."); // Displays a message mentioning the user's first name, favorite number, and the area of the circle.
